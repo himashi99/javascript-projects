af60d89ca23912c144b98697bd82b02a6a9a426b
Himashi Hettege Dona <himashi99@hotmail.com>
2018-06-01 11:53:05 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("done-button").onclick = function() {
+  var nameEntered = document.getElementById("name-input").value
+    document.getElementById("printed-name").innerHTML = "Hello " + nameEntered;
+};
+
+// document.getElementById("done-button").onClick = function() {
+//   alert("hello");
+// }
+// document.getElementById("name-input").value
+// console.log(nameEntered);
+// console.log("hello");
